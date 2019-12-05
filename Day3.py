922dff0665afda443c2e8d440e0961c8b29ce751
Stefan Kempe <stefan_kempe@gmx.de>
2019-12-05 22:58:46 +0100

    added Day3

--- Day3.py
+++ Day3.py
@@ -3,59 +3,68 @@
 with open("Day3_input.txt", "r") as f:
     w1 = f.readline().rstrip("\n").split(",")
     w2 = f.readline().rstrip("\n").split(",")
-    #print(wire1)
 
 def get_path(wire):
     x_coord = 0
     y_coord = 0
-    grid = [[x_coord,y_coord]]
-    #print(grid)
-    #print("o",end="")
+    grid = [(x_coord,y_coord)]
     for path in wire:
         if path[0] == "R":
             for _ in range(0,int(path[1:])):
                 x_coord += 1
-                grid.append([x_coord,y_coord])
+                grid.append((x_coord,y_coord))
         elif path[0] == "L":
             for _ in range(0,int(path[1:])):
                 x_coord -= 1
-                grid.append([x_coord,y_coord])
+                grid.append((x_coord,y_coord))
         elif path[0] == "U":
             for _ in range(0,int(path[1:])):
                 y_coord += 1
-                grid.append([x_coord,y_coord])
+                grid.append((x_coord,y_coord))
         elif path[0] == "D": 
             for _ in range(0,int(path[1:])):
                 y_coord -= 1
-                grid.append([x_coord,y_coord])
+                grid.append((x_coord,y_coord))
         else:
             raise Exception("Unknown instruction")
-    #print(grid)
+        
     return grid
 
-def get_crossing(wire1, wire2):
-    intersections = []
+def get_all_intersections(wire1, wire2):
+    path1 = set(get_path(wire1))
+    path2 = set(get_path(wire2))
+    intersections = path1.intersection(path2)
+    intersections.discard((0,0))
+
+    return intersections
+
+def get_manhattan_distance(wire1, wire2):
     m_distances = []
+    intersections = get_all_intersections(wire1, wire2)
+    for value in intersections:
+        m_distances.append(abs(value[0]) + abs(value[1]))
+    return min(m_distances)
+
+def get_fewest_steps(wire1, wire2):
+    steplist = []
     path1 = get_path(wire1)
     path2 = get_path(wire2)
-    #print("path1:",path1)
-    #print("path2:",path2)
-    for coordinate in path1:
-        if coordinate in path2:
-            intersections.append(coordinate)
-            print(intersections)
+    intersections = get_all_intersections(wire1, wire2)
 
-    for intersection in intersections[1:]:
-        m_distances.append(abs(intersection[0]) + abs(intersection[1]))
-    print(min(m_distances))
-    return min(m_distances)
-    #print(path1 in path2)
+    for value in intersections:
+        steplist.append(path1.index(value) + path2.index(value))
 
+    return min(steplist)
 
 
 if __name__ == "__main__":
-    assert get_crossing(["R8","U5","L5","D3"], ["U7","R6","D4","L4"]) == 6
-    assert get_crossing(
+    assert get_manhattan_distance(["R8","U5","L5","D3"], ["U7","R6","D4","L4"]) == 6
+    assert get_manhattan_distance(
         ["R75","D30","R83","U83","L12","D49","R71","U7","L72"], 
         ["U62","R66","U55","R34","D71","R55","D58","R83"]) == 159
-    print(get_crossing(w1, w2))
+    assert get_fewest_steps(["R8","U5","L5","D3"], ["U7","R6","D4","L4"]) == 30
+    assert get_fewest_steps(
+        ["R75","D30","R83","U83","L12","D49","R71","U7","L72"], 
+        ["U62","R66","U55","R34","D71","R55","D58","R83"]) == 610
+    print("Manhattan Distance:", get_manhattan_distance(w1, w2))
+    print ("fewest steps to intersection:", get_fewest_steps(w1, w2))
